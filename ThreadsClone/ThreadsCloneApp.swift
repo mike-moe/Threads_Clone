941e2470ce9a3100e8cf4d7601eed02925f2d396
Mike-Moe <https://github.com/mike-moe>
2024-01-18 07:49:14 -0800

    Initial Commit

---
+++ ThreadsClone/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by Mahyar Moazezi on 1/18/24.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
